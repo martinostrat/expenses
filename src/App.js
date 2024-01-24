e8e7d8e288cffc1a3c3a6017d36db07a0d8a6615
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-24 18:05:30 +0200

    Adds data to components via props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(),
+      title: 'New Book',
+      price: 30.99
+    },
+    {
+      date: new Date(),
+      title: 'Soup',
+      price: 2.49
+    }
+  ]
+
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem
+        expenseData={expenses[0]}
+      />
+      <ExpenseItem
+        expenseData={expenses[1]}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './expenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{props.expenseData.date.toString()}</div>
             <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2>{props.expenseData.title}</h2>
+                <div className='expense-item__price'>{props.expenseData.price}</div>
             </div>
         </div>
     );
