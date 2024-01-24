f0ee0f2676a81139ca7793e47c85ff440f79c079
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-24 18:19:10 +0200

    created separate component for date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
 import './expenseItem.css';
+import './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <div>{props.expenseData.date.toString()}</div>
+            <ExpenseDate
+                date={props.expenseData.date}
+            />
             <div className='expense-item__description'>
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.price}</div>
