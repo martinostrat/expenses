c8ea8da8ae5d66001b1c94fdb57247e4a9776b2b
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-29 19:15:53 +0200

    Created form to add new items to the list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -19,6 +20,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <NewExpense></NewExpense>
       <Expenses
         expenseData={expenses}
       />
