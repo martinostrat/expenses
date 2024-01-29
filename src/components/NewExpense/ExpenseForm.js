fbb281fb2ac40695c272fc787cf005063f053390
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-29 19:52:01 +0200

    git state hook refactor

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,19 +3,33 @@ import { useState } from 'react';
 
 const ExpenseForm = (props) => {
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
+
+    console.log(userInput);
 
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        })
     }
+
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-        console.log(enteredAmount)
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        })
     }
+
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        })
     }
 
     return (
