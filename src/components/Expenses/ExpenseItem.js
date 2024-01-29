34d725ec4cf00d49ed3d068967daad2786b2e6e3
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-29 18:55:59 +0200

    testing state hooks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,17 @@
 import './expenseItem.css';
 import './ExpenseDate';
 import ExpenseDate from './ExpenseDate';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = (props) => {
+        console.log('Clicked!');
+        setTitle(`Updated by click ${title}`);
+        console.log(title);
+    }
 
     return (
         <div key={props.title} className='expense-item card'>
@@ -13,6 +22,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.price}</div>
             </div>
+            <button onClick={clickHandler}>Click Me!</button>
         </div>
     );
 }
