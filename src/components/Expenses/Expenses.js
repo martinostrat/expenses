a63d76416a559fd638ace1b315bd9cf0d5d956fd
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-30 10:54:28 +0200

    Sends data from ExpensesFilter to Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,27 @@
 import ExpenseItem from "./ExpenseItem";
 import './expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
     const expenseData = props.expenseData
 
+    const [filterYear, setFilterYear] = useState('2023');
+
+    const selectedFilterYearHandler = (year) => {
+        setFilterYear(year);
+        console.log('expenses fail:')
+    }
+    console.log(filterYear);
+
+
     return (
         <Card className='expenses'>
-
+            <ExpensesFilter
+                /* Selected year from ExpensesFilter */
+                selectedFilterYear={selectedFilterYearHandler}
+            />
             {expenseData.map(expense => {
                 return (<ExpenseItem
                     date={expense.date}
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+
+    const yearFilterHandler = (event) => {
+        const selectedYear = event.target.value;
+        props.selectedFilterYear(selectedYear);
+    }
+
+    return (
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+                <label>Filter by year</label>
+                <select defaultValue={'2023'} onChange={yearFilterHandler}>
+                    <option value='2023'>2023</option>
+                    <option value='2024'>2024</option>
+                    <option value='2025'>2025</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
